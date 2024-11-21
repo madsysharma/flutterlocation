fa59d9a2e6a69fbc4313677cc6716e4aa9967a0d
madsysharma <108534984+madsysharma@users.noreply.github.com>
2024-11-21 12:43:30 -0800

    Update FlutterLocationService.kt

--- packages/location/android/src/main/java/com/lyokone/location/FlutterLocationService.kt
+++ packages/location/android/src/main/java/com/lyokone/location/FlutterLocationService.kt
@@ -106,7 +106,7 @@ class BackgroundNotification(
         }
 
         if (notify) {
-            val notificationManager = NotificationManagerCompat.from(context)
+            val notificationManager = NotificationManagerCompat.from(this)
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU || ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED){
                 notificationManager.notify(notificationId, builder.build())
             } 
